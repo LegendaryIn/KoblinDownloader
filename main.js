e771a311de19b4eda4f9ac2524b455c7532eebe3
= <=>
2019-01-16 14:21:48 +0800

    solve encode issue

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
 
+let options = {
+    url: 'https://www.wenku8.net/modules/article/reader.php?aid=2084&cid=74366',
+    encoding: null,
+};
 
-request('https://www.wenku8.net/modules/article/reader.php?aid=2084&cid=74366', function(error, response, body){
-    let $ = cheerio.load(body);
-    console.log($('#content').text());
+request(options, function(error, response, body){
+    let html = iconv.decode(body, 'gbk');
+    processHtml(html);
 });
+
+function processHtml(responseBody){
+    let $ = cheerio.load(responseBody, {decodeEntities: false});
+    console.log($('#content').html());
+}
